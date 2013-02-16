c661ea8c6d3f7fb97642576bf017d8414519c477
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-02-16 19:17:15 +0100

    [fellowship] in settings.py: - added admin user information - added a login_redirect_url - set template path

--- fellowship/settings.py
+++ fellowship/settings.py
@@ -5,7 +5,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Flavio Tarsetti', 'Tarsetti.Flavio@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -66,6 +66,8 @@ print MEDIA_ROOT
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
+LOGIN_REDIRECT_URL = '/'
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -120,6 +122,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
