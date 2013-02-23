400addbace3c55556f5aabe8819340ee4264ea39
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-02-23 21:16:26 +0100

    [tumblelog] added apps calls to urls.py

--- tumblelog/urls.py
+++ tumblelog/urls.py
@@ -1,17 +1,27 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tumblelog.views.home', name='home'),
     # url(r'^tumblelog/', include('tumblelog.foo.urls')),
+    #----------------------
+    # Apps urls call
+    #----------------------
+    url(r'^$',  include('homepage.urls')),
+    url(r'^home/',  include('homepage.urls')),
 
+
+    #----------------------
+    # Admin url
+    #----------------------
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
