50e27e69179e3ec7e2db0d44207999d78e55fdaf
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-02-25 23:10:23 +0100

    [core] added Tag class to models.py

--- core/models.py
+++ core/models.py
@@ -8,6 +8,17 @@ from django.core.urlresolvers import reverse
 from mongoengine import *
 from mongoengine.django.auth import User
 
+class Tag(Document):
+	title = StringField(max_length=200, required=True)
+
+	def __unicode__(self):
+		return self.title
+
+	def posts_count(self):
+		return len(Post.objects(tags=self.id))
+
+	def posts_avg_length(self):
+		return Post.objects(tags=self.id).average('text_length')
 
 class Post(Document):
     created_at = DateTimeField(default=datetime.now)
