fcdd10306bb32ef657ced813b1545f286c972da5
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-02-27 23:06:46 +0100

    [homepage] updated urls.py as first page

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
+from core.views import PostListView
 
 urlpatterns = patterns('homepage.views',
-    url(r'^$',             'home',      name="home"),
+    #url(r'^$',             'home',      name="home"),
+    url(r'^$',             PostListView.as_view(),      name="post-list"),
 )
