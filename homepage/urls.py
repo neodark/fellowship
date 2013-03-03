cb35e6563957b11dcac9d785b4dfb01a64f3951e
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-03-03 23:01:12 +0100

    [homepage] updated urls.py with PostCreateView and TagListView

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-from core.views import PostListView
+from core.views import PostListView, PostCreateView, TagListView
 
 urlpatterns = patterns('homepage.views',
     #url(r'^$',             'home',      name="home"),
-    url(r'^$',             PostListView.as_view(),      name="post-list"),
+    url(r'^$', PostListView.as_view(),      name="post-list"),
+	url(r'^add/$', PostCreateView.as_view(), name='post-create'),
+	url(r'^tags/$', TagListView.as_view(), name='tag-list'),
 )
