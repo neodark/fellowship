54c45681d442f147ff13c825990b13c6e6e85afb
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-03-07 17:26:37 +0100

    [tumblelog] in settings.py: added new app: mongonaut (for admin GUI)

--- tumblelog/settings.py
+++ tumblelog/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mongonaut',
     'core',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
