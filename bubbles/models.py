2897ac1ee0c8b440100f54f656e13043328d509c
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-03-15 23:34:45 +0100

    [bubbles] updated models.py with Bubble class

--- bubbles/models.py
+++ bubbles/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+#from django.db import models
+from datetime import datetime
 
 # Create your models here.
+from mongoengine import *
+from mongoengine.django.auth import User
+
+from core.models import User
+
+class Bubble(Document):
+    description = StringField()
+    created_at = DateTimeField(default=datetime.now)
+    author = ReferenceField(User)
+    attendees = models.ManyToManyField()
+
