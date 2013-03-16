69000209853b36e881d0393d847c3dccab484077
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-03-16 22:10:29 +0100

    [bubbles] added mongoadmin.py

---
+++ bubbles/mongoadmin.py
@@ -0,0 +1,27 @@
+# myapp/mongoadmin.py
+
+# Import the MongoAdmin base class
+from mongonaut.sites import MongoAdmin
+
+# Import your custom models
+from core.models import User
+from bubbles.models import Bubble
+
+# Subclass MongoAdmin and add a customization
+class BubbleAdmin(MongoAdmin):
+    # Searches on the names
+    search_fields = ('description', 'first_name')
+    # list names
+    list_fields = ('title', "text", "is_published")
+    def has_view_permission(self, request):
+        return True
+    def has_edit_permission(self, request):
+        return True
+    def has_add_permission(self, request):
+        return True
+    def has_delete_permission(self, request):
+        return True
+
+# Instantiate the MongoAdmin class
+# Then attach the mongoadmin to your model
+Bubble.mongoadmin = BubbleAdmin()
