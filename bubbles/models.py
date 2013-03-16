c5c1d2789638bfb2d00b96fc2360593623d3c183
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2013-03-16 18:49:35 +0100

    [bubbles] updated models.py Bubble class

--- bubbles/models.py
+++ bubbles/models.py
@@ -9,10 +9,15 @@ from core.models import User
 
 class Bubble(Document):
     description = StringField()
+    text_length = IntField()
     created_at = DateTimeField(default=datetime.now)
     author = ReferenceField(User)
-    attendees = models.ManyToManyField()
+    attendees = ListField(ReferenceField(User))
+    latest = BooleanField(default=True)
 
     def __unicode__(self):
         return self.description
 
+    def save(self, *args, **kwargs):
+        self.text_length = len(self.description)
+        return super(Bubble, self).save(*args, **kwargs)
