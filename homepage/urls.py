9bd540464d64d19a91312c90e1adaf8e8e65aadf
Flavio Tarsetti <Tarsetti.Flavio@gmail.com>
2014-03-09 22:35:02 +0100

    [homepage] patched urls.py for Django 1.6

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.conf import settings
 
 from core.views import PostListView, PostCreateView, TagListView, PostDetailView, PostUpdateView, PostDeleteView
